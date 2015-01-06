6cfacba918ca6ee3f882e14a29c89a92e960fa6a
WorkPC <edgars.janicevs@accenture.com>
2015-01-06 10:52:16 +0200

    added new ios proj

---
+++ git_demo/git_demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  git_demo
+//
+//  Created by Janicevs, Edgars on 1/6/15.
+//  Copyright (c) 2015 EdgarYanichev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ git_demo/git_demo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  git_demo
+//
+//  Created by Janicevs, Edgars on 1/6/15.
+//  Copyright (c) 2015 EdgarYanichev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
